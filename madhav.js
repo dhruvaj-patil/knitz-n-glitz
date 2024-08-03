802e8d1de35aee833afc837d471c9f813d14ef29
Dhruvaj <dhruvaj.programmer@gmail.com>
2024-08-03 10:36:34 +0530

    Finished

--- madhav.js
+++ madhav.js
@@ -1,13 +1,77 @@
 function toggleOpenButton(ele) {
-    if (ele.classList.contains("open")) {
-        document.querySelectorAll(".answer-icon").forEach(obj => {
-            obj.classList.remove("close")
-            obj.classList.add("open")
-        })
-        ele.classList.remove("open")
-        ele.classList.add("close")
-    } else {
-        ele.classList.add("open")
-        ele.classList.remove("close")
-    }
+  if (ele.classList.contains("open")) {
+    document.querySelectorAll(".answer-icon").forEach((obj) => {
+      obj.classList.remove("close");
+      obj.classList.add("open");
+    });
+    ele.classList.remove("open");
+    ele.classList.add("close");
+  } else {
+    ele.classList.add("open");
+    ele.classList.remove("close");
+  }
 }
+
+function toggleNavbarSelection(ele) {
+    let selectedNavs = document.querySelectorAll(".navbar-selected");
+    console.log("🚀 ~ toggleNavbarSelection ~ selectedNavs:", selectedNavs)
+    let toggleMobileMenu = document.querySelector('button[data-bs-target="#navbarNavMobile"]');
+
+    selectedNavs.forEach((obj) => {
+        if (obj !== ele) {
+            obj.classList.remove('navbar-selected')
+        }
+    })
+    ele.classList.add('navbar-selected')
+    toggleMobileMenu.click();
+  }
+
+function menuIconSwitch(ele) {
+  console.log(ele);
+  let dropDownImages = ele.querySelectorAll(".menu-dropdown");
+//   console.log("🚀 ~ testFn ~ dropDownImages:", dropDownImages)
+  dropDownImages.forEach((ele) => {
+    if (ele.className.includes("close")) {
+      ele.classList.remove("close");
+      ele.classList.add("open");
+    } else {
+        ele.classList.remove("open");
+        ele.classList.add("close");
+    }
+  });
+}
+
+function scrollToTop(ele) {
+    window.scrollTo(0, 0);
+}
+
+const landingPageCarousel = document.querySelector('#landingPage')
+console.log("🚀 ~ landingPageCarousel:", landingPageCarousel)
+
+const carousel = new bootstrap.Carousel(landingPageCarousel, {
+  interval: 2500,
+  touch: true,
+  ride: 'carousel',
+  wrap: true
+})
+
+
+// const featureCarousel = document.querySelector('#landingPage')
+// console.log("🚀 ~ featureCarousel:", featureCarousel)
+
+// const carouselv2 = new bootstrap.Carousel(featureCarousel, {
+//   interval: 5000,
+//   touch: true,
+//   ride: 'carousel',
+//   wrap: true
+// })
+
+// const featureCarousel = document.querySelector('#feature')
+// console.log("🚀 ~ landingPageCarousel:", landingPageCarousel)
+
+// const carousel = new bootstrap.Carousel(landingPageCarousel, {
+//   interval: 5000,
+//   touch: true,
+//   ride: 'carousel',
+//   wrap: true
+// })
